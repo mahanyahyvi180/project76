66eb711ec34aae9bda8ac899871b6107024edc44
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:30:39 +0330

    Add files via upload

---
+++ ConsoleApp76/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secreyword = "giraffe";
+
+            string guess = "";
+
+            while (guess != secreyword)
+            {
+                Console.Write("Enter guess :");
+
+                guess = Console.ReadLine();
+            }
+            Console.Write("you win");
+
+            Console.ReadLine();
+        }
+    }
+}
